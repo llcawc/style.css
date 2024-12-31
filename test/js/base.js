eb200920ed28646fe153a1bf52cf1c3b71db879e
llcawc <hostmastername@gmail.com>
2024-12-31 12:54:41 +0300

    меняю colorswitcher

--- test/js/base.js
+++ test/js/base.js
@@ -1,6 +1,7 @@
 // main.js
 
-import '../../colormode/dist/colormode.js'
+import switcher from '../../colormode/dist/colormode.js'
 import '../../prism/dist/prism.js'
 
+switcher()
 console.log('code running ...')
